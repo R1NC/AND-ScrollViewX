6e3616380808bb50f188518e68bf34a75ba619e5
RincLiu <i@rincliu.com>
2013-12-09 22:59:54 +0800

    Applied formatter.

--- src/com/rincliu/library/widget/ScrollViewX.java
+++ src/com/rincliu/library/widget/ScrollViewX.java
@@ -22,123 +22,142 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
 
-public class ScrollViewX extends ScrollView{
-	private static final long DELAY=100;
-	private int currentScroll;
-	private Runnable scrollCheckTask;
-	
-	/**
-	 * 
-	 * @param context
-	 */
-	public ScrollViewX(Context context) {
-		super(context);
-		init();
-	}
-	
-	/**
-	 * 
-	 * @param context
-	 * @param attrs
-	 */
-	public ScrollViewX(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
-	
-	/**
-	 * 
-	 * @param context
-	 * @param attrs
-	 * @param defStyle
-	 */
-	public ScrollViewX(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init();
-	}
-	
-	private void init(){
-		scrollCheckTask = new Runnable() {
-			@Override
-	        public void run() {
-	            int newScroll = getScrollY();
-	            if(currentScroll==newScroll){
-					if(onScrollListener!=null){
-	                    onScrollListener.onScrollStopped();
-	                }
-				}else{
-					if(onScrollListener!=null){
-	                    onScrollListener.onScrolling();
-	                }
-	                currentScroll=getScrollY();
-	                postDelayed(scrollCheckTask, DELAY);
-	            }
-	        }
-	    };
-	    setOnTouchListener(new OnTouchListener(){
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-	            if (event.getAction() == MotionEvent.ACTION_UP) {
-	            	currentScroll = getScrollY();
-	                postDelayed(scrollCheckTask, DELAY);
-	            }
-	            return false;
-	        }
-	    });
-	}
-	
-	public interface OnScrollListener{
-		public void onScrollChanged(int x, int y, int oldX, int oldY);
-		public void onScrollStopped();
-		public void onScrolling();
-	}
-	
-	private OnScrollListener onScrollListener;
-	
-	/**
-	 * 
-	 * @param onScrollListener
-	 */
-	public void setOnScrollListener(OnScrollListener onScrollListener){
-		this.onScrollListener=onScrollListener;
-	}
-	
-	@Override
-	protected void onScrollChanged(int x, int y, int oldX, int oldY){
-		super.onScrollChanged(x, y, oldX, oldY);
-		if(onScrollListener!=null){
-			onScrollListener.onScrollChanged(x, y, oldX, oldY);
-		}
-	}
-	
-	/**
-	 * 
-	 * @param child
-	 * @return
-	 */
-	public boolean isChildVisible(View child){
-		if(child==null){
-			return false;
-		}
-		Rect scrollBounds = new Rect();
-		getHitRect(scrollBounds);
-		return child.getLocalVisibleRect(scrollBounds);
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean isAtTop(){
-		return getScrollY()<=0;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean isAtBottom(){
-		return getChildAt(getChildCount()-1).getBottom()+getPaddingBottom()
-				==getHeight()+getScrollY();
-	}
+public class ScrollViewX extends ScrollView
+{
+    private static final long DELAY = 100;
+
+    private int currentScroll;
+
+    private Runnable scrollCheckTask;
+
+    /**
+     * @param context
+     */
+    public ScrollViewX(Context context)
+    {
+        super(context);
+        init();
+    }
+
+    /**
+     * @param context
+     * @param attrs
+     */
+    public ScrollViewX(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    /**
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
+    public ScrollViewX(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init()
+    {
+        scrollCheckTask = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                int newScroll = getScrollY();
+                if (currentScroll == newScroll)
+                {
+                    if (onScrollListener != null)
+                    {
+                        onScrollListener.onScrollStopped();
+                    }
+                }
+                else
+                {
+                    if (onScrollListener != null)
+                    {
+                        onScrollListener.onScrolling();
+                    }
+                    currentScroll = getScrollY();
+                    postDelayed(scrollCheckTask, DELAY);
+                }
+            }
+        };
+        setOnTouchListener(new OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if (event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    currentScroll = getScrollY();
+                    postDelayed(scrollCheckTask, DELAY);
+                }
+                return false;
+            }
+        });
+    }
+
+    public interface OnScrollListener
+    {
+        public void onScrollChanged(int x, int y, int oldX, int oldY);
+
+        public void onScrollStopped();
+
+        public void onScrolling();
+    }
+
+    private OnScrollListener onScrollListener;
+
+    /**
+     * @param onScrollListener
+     */
+    public void setOnScrollListener(OnScrollListener onScrollListener)
+    {
+        this.onScrollListener = onScrollListener;
+    }
+
+    @Override
+    protected void onScrollChanged(int x, int y, int oldX, int oldY)
+    {
+        super.onScrollChanged(x, y, oldX, oldY);
+        if (onScrollListener != null)
+        {
+            onScrollListener.onScrollChanged(x, y, oldX, oldY);
+        }
+    }
+
+    /**
+     * @param child
+     * @return
+     */
+    public boolean isChildVisible(View child)
+    {
+        if (child == null)
+        {
+            return false;
+        }
+        Rect scrollBounds = new Rect();
+        getHitRect(scrollBounds);
+        return child.getLocalVisibleRect(scrollBounds);
+    }
+
+    /**
+     * @return
+     */
+    public boolean isAtTop()
+    {
+        return getScrollY() <= 0;
+    }
+
+    /**
+     * @return
+     */
+    public boolean isAtBottom()
+    {
+        return getChildAt(getChildCount() - 1).getBottom() + getPaddingBottom() == getHeight() + getScrollY();
+    }
 }
--- src/com/rincliu/library/widget/activity/MainActivity.java
+++ src/com/rincliu/library/widget/activity/MainActivity.java
@@ -8,35 +8,48 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		final ScrollViewX svx=new ScrollViewX(this);
-		setContentView(svx);
-		TextView tv=new TextView(this);
-		tv.setHeight(3000);
-		svx.addView(tv);
-		svx.setOnScrollListener(new OnScrollListener(){
-			@Override
-			public void onScrollChanged(int x, int y, int oldX, int oldY) {
-				android.util.Log.d("@","x:"+oldX+"->"+x+", y:"+oldY+"->"+y);
-			}
-			@Override
-			public void onScrollStopped() {
-				if(svx.isAtTop()){
-					Toast.makeText(MainActivity.this, "Stopped at top", Toast.LENGTH_SHORT).show();
-				}else if(svx.isAtBottom()){
-					Toast.makeText(MainActivity.this, "Stopped at bottom", Toast.LENGTH_SHORT).show();
-				}else{
-					Toast.makeText(MainActivity.this, "Stopped", Toast.LENGTH_SHORT).show();
-				}
-			}
-			@Override
-			public void onScrolling() {
-				android.util.Log.d("@","scrolling...");
-			}
-		});
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        final ScrollViewX svx = new ScrollViewX(this);
+        setContentView(svx);
+        TextView tv = new TextView(this);
+        tv.setHeight(3000);
+        svx.addView(tv);
+        svx.setOnScrollListener(new OnScrollListener()
+        {
+            @Override
+            public void onScrollChanged(int x, int y, int oldX, int oldY)
+            {
+                android.util.Log.d("@", "x:" + oldX + "->" + x + ", y:" + oldY + "->" + y);
+            }
+
+            @Override
+            public void onScrollStopped()
+            {
+                if (svx.isAtTop())
+                {
+                    Toast.makeText(MainActivity.this, "Stopped at top", Toast.LENGTH_SHORT).show();
+                }
+                else if (svx.isAtBottom())
+                {
+                    Toast.makeText(MainActivity.this, "Stopped at bottom", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Stopped", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onScrolling()
+            {
+                android.util.Log.d("@", "scrolling...");
+            }
+        });
+    }
 }
